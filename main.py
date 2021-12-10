0532b0e62c8bedfdaa8634dd98a174f0c9a6f677
naftacabsi <springtrp87@gmail.com>
2021-12-11 02:46:56 +0300

    more pages

--- main.py
+++ main.py
@@ -7,4 +7,32 @@ app = Flask('')
 def main():
   return render_template("index.html")
 
+@app.route('/whatishaunted')
+def wih():
+  return render_template("whatishaunted.html")
+
+@app.route('/persik')
+def persik():
+  return render_template("persik.html")
+
+@app.route('/vanirka')
+def vanirka():
+  return render_template("vanirka.html")
+
+@app.route('/wtxil')
+def wtxil():
+  return render_template("wtxil.html")
+
+@app.route('/ilya')
+def ilya():
+  return render_template("ilya.html")
+
+@app.route('/deathkiller')
+def death():
+  return render_template("death.html")
+
+@app.route('/steve')
+def steve():
+  return render_template("steve.html")
+
 app.run('0.0.0.0', 2575)
